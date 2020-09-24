a875faf389550094e9b999eca6fcf5d5ad69dadf
harry-guava <harry_li@mail.chungyo.net>
2020-09-24 18:02:24 +0800

    驗證完成 資料庫損壞中

---
+++ shopping/database/migrations/2020_09_24_070316_change_users_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ChangeUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    //up執行時對資料庫異動的地方
+    public function up()
+    {
+        //
+        Schema::table('users', function(Blueprint $table){
+            //在欄位後新增欄位
+            $table->string('identcard')->after('password');
+            //$table->string('address')->after('identcard');
+            $table->string('phone')->after('identcard');
+            
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    //down撤銷異動
+    public function down()
+    {
+        //
+        Schema::table('users', function(Blueprint $table){
+            $table->dropColumn('phone');
+            $table->drop('address');
+            $table->dropColumn('identcard');
+        });
+    }
+}
---
+++ shopping/resources/views/auth/register.blade.php
@@ -0,0 +1,104 @@
+@extends('layouts.app')
+
+@section('content')
+<div class="container">
+    <div class="row">
+        <div class="col-md-8 col-md-offset-2">
+            <div class="panel panel-default">
+                <div class="panel-heading">Register</div>
+
+                <div class="panel-body">
+                    <form class="form-horizontal" method="POST" action="{{ route('register') }}">
+                        {{ csrf_field() }}
+
+                        <div class="form-group{{ $errors->has('name') ? ' has-error' : '' }}">
+                            <label for="name" class="col-md-4 control-label">名稱</label>
+
+                            <div class="col-md-6">
+                                <input id="name" type="text" class="form-control" name="name" value="{{ old('name') }}" required autofocus>
+
+                                @if ($errors->has('name'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('name') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
+                            <label for="email" class="col-md-4 control-label">信箱</label>
+
+                            <div class="col-md-6">
+                                <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required>
+
+                                @if ($errors->has('email'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('email') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
+                            <label for="password" class="col-md-4 control-label">密碼</label>
+
+                            <div class="col-md-6">
+                                <input id="password" type="password" class="form-control" name="password" required>
+
+                                @if ($errors->has('password'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('password') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+
+                        <div class="form-group">
+                            <label for="password-confirm" class="col-md-4 control-label">再次輸入密碼</label>
+
+                            <div class="col-md-6">
+                                <input id="password-confirm" type="password" class="form-control" name="password_confirmation" required>
+                            </div>
+                        </div>
+
+                        <div class="form-group{{ $errors->has('identcard') ? ' has-error' : '' }}">
+                            <label for="identcard" class="col-md-4 control-label">身分證號碼</label>
+
+                            <div class="col-md-6">
+                                <input id="identcard" type="text" class="form-control" name="identcard"  pattern="^[A-Z]\d{9}$" value="{{ old('identcard') }}" required>
+
+                                @if ($errors->has('identcard''))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('identcard') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        
+                        <div class="form-group{{ $errors->has('phone') ? ' has-error' : '' }}">
+                            <label for="phone" class="col-md-4 control-label">電話</label>
+
+                            <div class="col-md-6">
+                                <input id="phone" type="tel" class="form-control" name="email"  pattern="(^(\d{3,4}-)?\d{7,8})$|(13[0-9]{9})" value="{{ old('phone') }}" required>
+
+                                @if ($errors->has('phone'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('phone') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+                        </div>
+                        <div class="form-group">
+                            <div class="col-md-6 col-md-offset-4">
+                                <button type="submit" class="btn btn-primary">
+                                    Register
+                                </button>
+                            </div>
+                        </div>
+                    </form>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
